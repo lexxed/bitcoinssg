161e541a2d6c8d2e83d9555ee900d0f1489a71f0
lexxed <lexx.tham@gmail.com>
2018-11-16 02:18:45 +0000

    pause screen

--- resources/views/pause-screen.blade.php
+++ resources/views/pause-screen.blade.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Go</title>
+    <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+    <meta name="description" content="Go">
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- Store CSRF token for AJAX calls -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" id="bulma" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.3.2/css/bulma.min.css" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Favicon and Apple Icons -->
+    <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
+</head>
+<body>
+
 <br><br><br><br>
 <div align="center" valign="middle">
 <br>
@@ -9,3 +31,6 @@ Please wait while we transfer you to {{ ucfirst($affiliate->name) }}
 <br><br>
 Sign up with {{ ucfirst($affiliate->name) }} to buy your first bitcoin.
 </div>
+
+</body>
+</html>
